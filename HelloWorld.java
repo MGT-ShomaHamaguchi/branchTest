729cc145c3457005e18c18e28c6bd805e6d8cf07
Shoma-Hamaguchi <hamaguti-shoma@mgt-net.com>
2025-03-11 10:39:45 +0900

    Revert "Merge branch '案件1'"
    This reverts commit 21eed861af4b8f4fe7ec132c301ca4bc387125a3, reversing
    changes made to ceaa88370f1c45a1571ab33b49ac635ae3b87ebc.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("subject1");
-    	System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
     	System.out.println("subject2");
     }
 }
